57cfd60463b3a0f1b3d18e2108f641a189f371b2
Kitsor3000 <krostik2007@gmail.com>
2024-09-15 15:49:33 +0300

    Add project files.

---
+++ Was/Was.cpp
@@ -0,0 +1,11 @@
+// Was.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 3, b = 4;
+    cout << a + b;
+}
